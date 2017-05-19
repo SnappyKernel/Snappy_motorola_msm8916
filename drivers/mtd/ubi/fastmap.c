6d2f4c05de8ddf8f879344b4363211794cec43ed
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-05-19 08:24:10 -0500

    fix

--- drivers/mtd/ubi/fastmap.c
+++ drivers/mtd/ubi/fastmap.c
@@ -465,17 +465,9 @@ static int scan_pool(struct ubi_device *ubi, struct ubi_attach_info *ai,
 			dbg_bld("Adding PEB to free: %i", pnum);
 
 			if (err == UBI_IO_FF_BITFLIPS)
-<<<<<<< HEAD
-				add_aeb(ai, free, pnum, ec, last_erase_time,
-						0, 1);
-			else
-				add_aeb(ai, free, pnum, ec, last_erase_time,
-						0, 0);
-=======
 				scrub = 1;
 
 			add_aeb(ai, free, pnum, ec, scrub);
->>>>>>> 020d1c5... Linux 3.10.105
 			continue;
 		} else if (err == 0 || err == UBI_IO_BITFLIPS) {
 			dbg_bld("Found non empty PEB:%i in pool", pnum);
